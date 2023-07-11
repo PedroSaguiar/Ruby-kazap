e50a8f39e101891c35ba02eb1757915cbd3353de
Pedro Aguiar <ppaulo.saguiar@gmail.com>
2023-07-10 21:05:09 -0300

    ruby exercicio 10/07

---
+++ exercicio_aula1.rb
@@ -0,0 +1,70 @@
+#1
+nome= "Pedro"
+idade = 18
+
+puts "ola, meu nome eh #{nome} e tenho #{idade} anos "
+
+#2
+x= 5
+y=2
+
+puts "o valor de x eh #{x} e seu dobro eh #{x * y}"
+
+#3
+x= 9
+y=6
+
+puts "o valor de x eh #{x} e o valor de y eh #{y} e sua soma  eh #{x + y}"
+
+#4
+x= 7
+y=7
+
+puts "o valor de x eh #{x} e o valor de y eh #{y} e seu produto eh #{x * y}"
+
+#5
+x= 5
+y=2
+
+puts "o valor de x eh #{x} e o valor de y eh #{y} e sua subtração eh #{x - y}"
+
+#6
+x= 12
+y=6
+
+puts "o valor de x eh #{x} e o valor de y eh #{y} e sua divisão eh #{x / y}"
+
+#7
+x= 9
+
+puts "o valor de x eh #{x} e seu valor quando elevado ao quadrado eh #{x ** 2}"
+
+#8
+
+x= 11
+y=2
+
+puts "o valor de x eh #{x} e o valor de y eh #{y} e o resto de sua divisão eh #{x % y}"
+
+#9
+
+x=10
+y= nil
+puts "a classe de x eh #{x.class}, e seu valor eh #{x}, e convertendo para uma string fica #{y=x.to_s} e sua classe eh #{y.class}"
+
+#10
+x="8"
+y= nil
+puts "o valor de x eh #{x} e seu tipo eh #{x.class} e convertendo para uma string fica #{y=x.to_i} e sua classe eh #{y.class}"
+
+#11
+
+x=10.5
+y= nil
+puts "a classe de x eh #{x.class}, e seu valor eh #{x}, e convertendo para uma string fica #{y=x.to_s} e sua classe eh #{y.class}"
+
+#12
+
+x="9"
+y= nil
+puts "o valor de x eh #{x} e seu tipo eh #{x.class} e convertendo para um ponto flutuante fica #{y=x.to_f} e sua classe eh #{y.class}"
